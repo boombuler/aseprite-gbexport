20d2522e4f2301f47c8c02c0c1242dfda72d36b0
boombuler <boombuler@googlemail.com>
2020-04-23 17:15:40 +0200

    prevent export of duplicates + add tilemap

--- GameboyExport.lua
+++ GameboyExport.lua
@@ -1,7 +1,7 @@
 local sprite = app.activeSprite
 
 
--- Check constraints
+-- Check constrains
 if sprite == nil then
   app.alert("No Sprite...")
   return
@@ -22,48 +22,88 @@ if (sprite.height % 8) ~= 0 then
 end
 
 local function getTileData(img, x, y)
-	local res = ""
+    local res = ""
 
-	for  cy = 0, 7 do
-		local hi = 0
-		local lo = 0
+    for  cy = 0, 7 do
+        local hi = 0
+        local lo = 0
 
-		for cx = 0, 7 do
-			px = img:getPixel(cx+x, cy+y)
-			
+        for cx = 0, 7 do
+            px = img:getPixel(cx+x, cy+y)
+            
 
-			if (px & 1) ~= 0 then
-				lo = lo | (1 << 7-cx)
-			end
-			if (px & 2) ~= 0 then
-				hi = hi | (1 << 7-cx)
-			end
-		end
-		res = res .. string.format("$%02x, $%02x", lo, hi)
-		if cy < 7 then
-			res = res .. ", "
-		end
-		
-	end
+            if (px & 1) ~= 0 then
+                lo = lo | (1 << 7-cx)
+            end
+            if (px & 2) ~= 0 then
+                hi = hi | (1 << 7-cx)
+            end
+        end
+        res = res .. string.format("$%02x, $%02x", lo, hi)
+        if cy < 7 then
+            res = res .. ", "
+        end
+        
+    end
 
-	return "DB " .. res .. "\n"
+    return "DB " .. res .. "\n"
 end
 
-local function exportFrame(frm)
-	if frm == nil then
-		frm = 1
-	end
+local spriteLookup = {}
+local lastLookupId = 0
 
-	local img = Image(sprite.spec)
-	img:drawSprite(sprite, frm)
+local function exportFrame(useLookup, frm)
+    if frm == nil then
+        frm = 1
+    end
 
-	for x = 0, sprite.width-1, 8 do
-		for y = 0, sprite.height-1, 8 do
-			io.write(getTileData(img, x, y))
-		end
-	end
+    local img = Image(sprite.spec)
+    img:drawSprite(sprite, frm)
+
+    local result = {}
+
+    for x = 0, sprite.width-1, 8 do
+        local column = {}
+        for y = 0, sprite.height-1, 8 do
+            local data = getTileData(img, x, y)
+            local id = 0
+            if useLookup then
+                id = spriteLookup[data]
+                if id == nil then
+                    id = lastLookupId + 1
+                    lastLookupId = id
+
+                    spriteLookup[data] = id
+                else
+                    data = nil
+                end 
+            else
+                id = lastLookupId + 1
+                lastLookupId = id
+            end
+            table.insert(column, id)
+            if data ~= nil then
+                io.write(data)
+            end
+        end
+        table.insert(result, column)
+    end
+
+    return result
 end
 
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
 
 local dlg = Dialog()
 dlg:file{ id="exportFile",
@@ -73,26 +113,66 @@ dlg:file{ id="exportFile",
           save=true,
         --filename= p .. fn .. "asm",
           filetypes={"asm", "inc", "z80" }}
+dlg:file{ id="mapFile",
+          label="Tile-Map-File",
+          title="Gameboy-Assembler Export",
+          open=false,
+          save=true,
+        --filename= p .. fn .. "asm",
+          filetypes={"asm", "inc", "z80" }}
+
 dlg:check{ id="onlyCurrentFrame",
            text="Export only current frame",
            selected=true }
+dlg:check{ id="removeDuplicates",
+           text="Remove duplicate tiles",
+           selected=false}
 
 dlg:button{ id="ok", text="OK" }
 dlg:button{ id="cancel", text="Cancel" }
 dlg:show()
 local data = dlg.data
 if data.ok then
-	local f = io.open(data.exportFile, "w")
-	io.output(f)
+    local f = io.open(data.exportFile, "w")
+    io.output(f)
 
-	if data.onlyCurrentFrame then
-		exportFrame(app.activeFrame)
-	else
-	 	for i = 1,#sprite.frames do
-	 		io.write(string.format(";Frame %d\n", i))
-	 		exportFrame(i)
-		end
-	end
+    local mapData = {}
 
-	io.close(f)
+    if data.onlyCurrentFrame then
+        table.insert(mapData, exportFrame(data.removeDuplicates, app.activeFrame))
+    else
+        for i = 1,#sprite.frames do
+            io.write(string.format(";Frame %d\n", i))
+            table.insert(mapData, exportFrame(data.removeDuplicates, i))
+        end
+    end
+
+    io.close(f)
+
+    if data.mapFile ~= nil then
+        
+        local mf = io.open(data.mapFile, "w")
+
+
+        for frameNo, frameMap in ipairs(mapData) do 
+            if #mapData > 1 then
+                mf:write(string.format(";Frame %d\n", frameNo))
+            end
+
+            for y = 1, #frameMap[1] do
+                mf:write("DB ")
+                for x = 1, #frameMap do
+                    if x > 1 then
+                        mf:write(", ")
+                    end
+
+                    mf:write(string.format("$%02x", frameMap[x][y]))
+                end
+                mf:write("\n")
+            end
+        end
+        mf:close()
+    end
+
+    
 end
